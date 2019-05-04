48ea28ee90f6374e3d743aa43695cf306edea3c5
dimple23 <dimplechavda23@gmail.com>
2019-05-04 14:15:49 -0400

    added

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require("express");
 var path = require("path");
 var connection = require("./db/connection");
 
-// Tells node that we are creating an "express" server
+
 var app = express();
-// Sets an initial port. We"ll use this later in our listener
+// Sets an initial port
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
@@ -68,6 +68,6 @@ app.listen(PORT, function() {
 });
 
 
-
+ç
 
 
